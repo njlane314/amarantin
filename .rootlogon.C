fc1b8c050cbe5e0a6af9ab6b5d5522deb2fa4b69
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-26 20:30:40 +0000

    Delete .rootlogon.C

--- .rootlogon.C
+++
@@ -1,9 +0,0 @@
-#include "TString.h"
-#include "TSystem.h"
-
-{
-    TString include_flag = "-Iframework/io/include";
-    TString include_path = gSystem->GetIncludePath();
-    if (!include_path.Contains(include_flag))
-        gSystem->AddIncludePath(include_flag);
-}
